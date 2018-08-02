882aaa8c63b5c5b564bf48c883509bbdcc2b9ae7
Valeriy Shinkevich <v.v.shinkevich@gmail.com>
2018-08-03 00:42:17 +0300

    Add semigroup syntax

--- src/main/scala/fpspeedrun/syntax.scala
+++ src/main/scala/fpspeedrun/syntax.scala
@@ -18,7 +18,18 @@ object syntax {
     // Добавляем всем объектам, для которых определен (в зоне видимости) ord:Ord[T], метод compare и оператор ===
     implicit class OrdOps[T](val x: T) extends AnyVal {
       def compare(y: T)(implicit ord: Ord[T]): CompareResult = ord.compare(x, y)
+
       def ===(y: T)(implicit ord: Ord[T]): Boolean = ord.===(x, y)
+    }
+
+  }
+
+  object semigroup {
+
+    implicit class SemiGroupOps[T](val x: T) extends AnyVal {
+      def combine(y: T)(implicit sg: SemiGroup[T]): T = sg.combine(x, y)
+
+      def |+|(y: T)(implicit sg: SemiGroup[T]): T = combine(y)
     }
 
   }
