0e42fc2c1d4aa5a802de5a1a366a047df740ee80
Valeriy Shinkevich <v.v.shinkevich@gmail.com>
2018-08-07 11:36:03 +0300

    Add Monoid with laws

--- src/main/scala/fpspeedrun/Monoid.scala
+++ src/main/scala/fpspeedrun/Monoid.scala
@@ -1,18 +1,19 @@
 package fpspeedrun
 
 trait Monoid[F] {
-
-  def zero: F
-  def op(x: F, y: F): F
-
+  def empty: F
+  def op(a: F, b: F): F
 }
 
 object Monoid {
 
-      object Laws {
-        def associativity[F:Monoid[_]](x: F, y: F, z: F)(implicit F:Eq[F]): Boolean = {
+  object Laws {
+    def associativity[T](x: T, y: T, z: T)(implicit m: Monoid[T]): Boolean = {
+      m.op(m.op(x, y), z) == m.op(x, m.op(y, z))
+    }
 
-        }
-      }
-
+    def identity[T](x: T)(implicit m: Monoid[T]): Boolean = {
+      m.op(m.empty, x) == x
+    }
+  }
 }
